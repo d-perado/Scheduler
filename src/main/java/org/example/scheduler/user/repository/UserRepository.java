1ee8ee4a3fa52628ffb2cef2bda8901597c1a8cb
d-perado <sjm01065@naver.com>
2025-11-17 17:41:46 +0900

    유저 커스텀 레포지토리 삭제

--- src/main/java/org/example/scheduler/user/repository/UserRepository.java
+++ src/main/java/org/example/scheduler/user/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findUserByEmail(String email);
